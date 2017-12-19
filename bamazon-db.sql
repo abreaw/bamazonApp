b1702e1a2b86d07669fc999ffb474b9063f2172a
Brea Torres <abreaw328@gmail.com>
2017-12-18 20:30:43 -0500

    results of inventory view lined up

--- bamazon-db.sql
+++ bamazon-db.sql
@@ -15,7 +15,7 @@ CREATE TABLE products(
 USE bamazon;
 
 INSERT INTO products (product_name,department_name,price,stock_quantity) VALUES
-  ('Christmas Lights','Seasonal',25.99,250), ('Snowman Ornament','Seasonal',4.99,100), ('Glitter Stocking', 'Seasonal',15.99,50), ('Harry Potter Sock Advent Calendar','Seasonal',19.99,10),
+  ('Christmas Lights','Seasonal',25.99,250), ('Snowman Ornament','Seasonal',4.99,100), ('Glitter Stocking', 'Seasonal',15.99,50), ('Harry Potter Calendar','Seasonal',19.99,10),
   ('5 Foot Christmas Tree','Seasonal',63.99,25), ('Very Merry Felt Banner','Seasonal',5.00,30), ('Awkward Turtle','Toys',5.99,15), ('Panic Button Desk Bell','Toys',2.99,15),
   ('Caramel Pieces - 10 pack','Candy',4.89,25), ('Holiday Santa Gift Bag','Seasonal',1.99,35), ('Merlot','Alcohol',12.99,400), ('Shiraz','Alcohol',9.99,250), ('Pinot Noir','Alcohol',10.99,250),
   ('Spiral Cut Ham','Meat',19.99,25), ('Pecans - Whole','Produce',3.99,20), ('Walnuts - Whole','Produce',3.99,20), ('Gift Cards','Gifts',25.00,200);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,7 +22,7 @@ var connection = mysql.createConnection({
 connection.connect( function(error) {
 
 	if (error) throw error;
-	console.log("connection as ID = " + connection.threadId);
+	// console.log("connection as ID = " + connection.threadId);
 	// console.log(connection);
 
 	// function called when connection to the database made
@@ -37,10 +37,76 @@ function displayInventory() {
 	connection.query("SELECT * FROM `products`", function (err, results, fields) {
 		if (err) throw err;
 		
+		console.log("");
+		console.log("ID    Product Name                 Price   ");
 		console.log("-------------------------------------------");
-		console.log(results);
+		// console.log(results);
+
+		// loop through the results from the select query on the products table
+		for (var i = 0; i < results.length; i++) {
+			console.log(formatTableData(results[i].item_id, results[i].product_name, results[i].price));
+		}
+
 		console.log("-------------------------------------------");
 		connection.end();  // might need to put this elsewhere to get the connection to run properly
+
+		// console.log(formatTableData(results[0].item_id, results[0].product_name, results[0].price));
+
 	});
 
 }
+
+
+// ---------------------------------------------------------------------------------------------------------------
+// Create & Format table row data
+// arguments: product id, product name, product price
+// returns: string with the data setup and ready to be displayed
+// ---------------------------------------------------------------------------------------------------------------
+function formatTableData(id, name, price) {
+
+	// set the max length for the id column on the display view
+	var maxIdLength = 2;
+
+	// change id to a string for length property to work
+	var idString = id.toString();  
+
+	// console.log("length for id " + idString + " = " + idString.length);
+
+	// check to see if the entry is smaller than the max length
+	if (idString.length < maxIdLength) {
+
+		// calculate the right spacing for the column display
+		var spaceIdLength = maxIdLength - idString.length;
+
+		// console.log("spaceIdLength for " + idString + " = " + spaceIdLength);
+
+		// loop through to add the right amount of space to line up the column information
+		for (var i = 0; i < spaceIdLength; i++) {
+			idString += " ";
+		}
+	}
+
+
+	// set the max length for the name column on the display view
+	var maxNameLength = 27;
+
+	// check to see if the entry is smaller than the max length
+	if (name.length < maxNameLength) {
+
+		// calculate the right spacing for the column display
+		var spaceLength = maxNameLength - name.length;
+
+		// console.log("spaceLength for " + name + " = " + spaceLength);
+
+		// loop through to add the right amount of space to line up the column information
+		for (var i = 0; i < spaceLength; i++) {
+			name += " ";
+		}
+	}
+	
+	// use ` instead of ' or " to be able to add the variable names into the string and it interpret them for the values passed in
+	return `${idString}    ${name}  ${price}	`;
+
+}
+
+
